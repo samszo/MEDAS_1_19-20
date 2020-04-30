8fc8c3f28f83635b4c91556379ddf837c7e50161
alixvd <61273792+alixvd@users.noreply.github.com>
2020-04-30 10:16:59 +0200

    Add files via upload

---
+++ MANSUETO Alix/mydb.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  jeu. 30 avr. 2020 à 10:11
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP :  7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `oeuvres`
+--
+
+CREATE TABLE `oeuvres` (
+  `ID` int(11) NOT NULL,
+  `Nom` varchar(45) DEFAULT NULL,
+  `Année de parution` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `prix nobel`
+--
+
+CREATE TABLE `prix nobel` (
+  `ID` int(11) NOT NULL,
+  `Année d'obtention` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `oeuvres`
+--
+ALTER TABLE `oeuvres`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Index pour la table `prix nobel`
+--
+ALTER TABLE `prix nobel`
+  ADD PRIMARY KEY (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
